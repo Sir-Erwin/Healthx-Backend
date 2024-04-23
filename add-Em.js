96bc7cbcc8bc9391118267e7fbca3d74d25ee18e
DimiO1 <119295709+DimiO1@users.noreply.github.com>
2024-04-23 00:26:12 -0500

    all files

---
+++ add-Em.js
@@ -0,0 +1,76 @@
+// backend/server.js
+
+const http = require('http');
+const url = require('url');
+const mysql = require('mysql');
+
+// MySQL database connection configuration
+const db = mysql.createConnection({
+  host: "nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "vf3kex8mlhqctqd0", password: "eyi36qurfvsqlnuj",
+    database: "wb1uv6dqlflyabu2",
+});
+
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to MySQL database');
+});
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allow specified HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Allow additional headers
+
+  //const { method, url: reqUrl } = req;
+  //const parsedUrl = url.parse(reqUrl, true);
+
+  // Route request based on URL and HTTP method
+  if (req.method === 'GET' && req.url === '/addEm') {
+    // Serve the HTML form
+    fs.readFile('add-employee.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+            return;
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+  } else if (req.method === 'POST' && req.url=== '/addEm') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const data = JSON.parse(body);
+
+      const {eid, fname, lname, email, gender, number, role } = data;
+      // Insert data into MySQL table
+      //const sql = 'INSERT INTO your_table (column_name) VALUES (?)';
+      db.query('INSERT INTO EMPLOYEE (EID, FName, LName, Contact, Gender, PhoneNum, Role) VALUES (?, ?, ?, ?, ?, ?, ?)', [eid, fname, lname, email, gender, number, role], (err, result) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Error saving data' }));
+          throw err;
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Data saved successfully' }));
+      });
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Not found' }));
+  }
+});
+
+const port = 3000;
+
+// Start server
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
