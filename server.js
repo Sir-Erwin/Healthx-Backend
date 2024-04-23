dc3ba0066c1df5d9fb156672fd6a1c94fa0cc2f4
Erwin Puthoor Manoj <ErwinManoj@gmail.com>
2024-04-23 13:06:16 -0500

    stuff

--- server.js
+++ server.js
@@ -38,7 +38,9 @@ const server = http.createServer((req, res) => {
   }
 
 const { pathname, query } = url.parse(req.url, true);
-
+/**
+ * Employee Login
+ */
 if (pathname === '/emp_login' && req.method === 'POST') {
   let body = '';
   req.on('data', (chunk) => {
@@ -88,6 +90,116 @@ if (pathname === '/emp_login' && req.method === 'POST') {
       });
     });
   });
+}
+
+/**
+ * Patient Login
+ */
+else if (pathname === '/pat_login' && req.method === 'POST') {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+  req.on('end', () => {
+    const { username, password } = JSON.parse(body);
+      
+    // Bad Request
+    if (!username || !password) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Missing username or password');
+      return;
+    }
+
+    // Check if user exists in the database
+    db.query('select * from patlogindetails WHERE Email = ?', [username], (err, results) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Query Err: Internal server error');
+        return;
+      }
+      if (results.length === 0) { // User Doesn't Exist
+        res.writeHead(401, { 'Content-Type': 'text/plain' });
+        res.end('Invalid username or password');
+        return;
+      }
+      
+      // Compare hashed password
+      const user = results[0];
+      console.log("Username: " + username + "\nPassword: " + password + "\nHashedPassw: " + user.Passw + "\n");
+            
+      bcrypt.compare(password, user.Passw, (bcryptErr, bcryptResult) => {
+        if (bcryptErr) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.write('Bcrypt Err: Internal server error');
+          res.end(JSON.stringify(bcryptErr));
+          return;
+        }
+        if (!bcryptResult) {
+          res.writeHead(401, { 'Content-Type': 'text/plain' });
+          res.end('Invalid username or password');
+          return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(JSON.stringify(user.PID));
+      });
+    });
+  });
+}
+
+/**
+ * Employee Creation
+ */
+else if (pathname === '/emp_signup' && req.method === 'POST'){
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    // Parse form data
+    const formData = JSON.parse(body);
+    const fname = formData.fname;
+    const mname = formData.Mname;
+    const lname = formData.Lname;
+    const email = formData.email;
+    const number = formData.number;
+    const social = formData.ssn;
+    const role = formData.role
+    const gender = formData.gender;
+    var eid = (formData.eid) ? formData.eid : 8009;
+    var password = (formData.password) ? formData.password : "Password!";
+
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+
+      if (err) {
+        console.log('\x1B[31Error while hashing password:', err);
+        return;
+      } password = hashedPassword; 
+   }).then(pass => { 
+    if(!formData.eid) {
+      db.query('SELECT EID FROM EMPLOYEE', (err, results) => {
+        eid = results[results.length - 1].EID+1;
+      });
+      password = pass;
+    }
+   }).then(passw, new_eid => {
+    // Example MySQL query (if you're using a database)
+    db.query('INSERT INTO EMPLOYEE (FName, MInitial, LName, Email, SSN, EID, Passw, PhoneNum, Role, Gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+    [fname, mname, lname, email, social, new_eid, passw, number, role, gender], (err, results) => {
+    if (err) {
+        console.error('Error storing data:', err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+    } else {
+        console.log('Data stored successfully');
+        res.writeHead(200, {'Content-Type': 'plain/text'});
+        res.end(eid.toString());
+        console.log(eid.toString());
+    }
+  });
+   });
+  });
+
 }
 
 else {
