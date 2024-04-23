cdafe8d91378ab0ec703109620b5ad94cbdfc962
Dimimu <119295709+DimiO1@users.noreply.github.com>
2024-04-23 16:03:13 -0500

    both everything together

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ else if (pathname === '/emp_signup' && req.method === 'POST'){
     var eid = (formData.eid) ? formData.eid : 8009;
     var password = (formData.password) ? formData.password : "Password!";
 
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
+    /*bcrypt.hash(password, 10, (err, hashedPassword) => {
 
       if (err) {
         console.log('\x1B[31Error while hashing password:', err);
@@ -197,19 +197,197 @@ else if (pathname === '/emp_signup' && req.method === 'POST'){
         console.log(eid.toString());
     }
   });
-   });
+   });*/
+   bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.log('\x1B[31Error while hashing password:', err);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    } 
+  
+    password = hashedPassword;
+  
+    // If new employee, get new employee ID
+    if (!formData.eid) {
+      db.query('SELECT EID FROM EMPLOYEE', (err, results) => {
+        if (err) {
+          console.error('Error getting new employee ID:', err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+          return;
+        }
+        eid = results[results.length - 1].EID + 1;
+  
+        // Insert data into EMPLOYEE table
+        db.query('INSERT INTO EMPLOYEE (FName, MInitial, LName, Email, SSN, EID, Passw, PhoneNum, Role, Gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+          [fname, mname, lname, email, social, eid, password, number, role, gender], (err, results) => {
+            if (err) {
+              console.error('Error storing data:', err);
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Internal Server Error');
+              return;
+            }
+            console.log('Data stored successfully');
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(eid.toString());
+            console.log(eid.toString());
+          });
+      });
+    } else { // If existing employee, insert data directly
+      db.query('INSERT INTO EMPLOYEE (FName, MInitial, LName, Email, SSN, EID, Passw, PhoneNum, Role, Gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+        [fname, mname, lname, email, social, formData.eid, password, number, role, gender], (err, results) => {
+          if (err) {
+            console.error('Error storing data:', err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            return;
+          }
+          console.log('Data stored successfully');
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(formData.eid.toString());
+          console.log(formData.eid.toString());
+        });
+    }
+  });
+  
+  });
+/* Adding Employees to the table */
+}
+else if (req.method === 'POST' && req.url=== '/addEm') {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
   });
 
-}
+  req.on('end', () => {
+    const data = JSON.parse(body);
 
+    const {eid, fname, lname, email, gender, number, role } = data;
+    // Insert data into MySQL table
+    db.query('INSERT INTO EMPLOYEE (EID, FName, LName, Email, Gender, PhoneNum, Role) VALUES (?, ?, ?, ?, ?, ?, ?)', [eid, fname, lname, email, gender, number, role], (err, result) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Error saving data' }));
+        throw err;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Data saved successfully' }));
+    });
+  });
+}
+/*Displaying Employees on the table */
+else if (req.method === 'DELETE' && req.url.startsWith('/employees/')) {
+  // Extract employee number from the URL
+  const number = req.url.split('/')[2];
+  // Delete the employee from the database
+  db.query('DELETE FROM EMPLOYEE WHERE PhoneNum = ?', [number], (error, results) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Delete Internal Server Error' }));
+      } else {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ message: 'Employee deleted successfully' }));
+      }
+  });
+} else if (req.url === '/employees') {
+  // Fetch employee details from MySQL database
+  db.query('SELECT EID, FName, LName, Email, Gender, PhoneNum, Role FROM EMPLOYEE', (error, results, fields) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Internal Server Error' }));
+          return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(results));
+  });
+}
+/*Displaying Doctors */
+else if (req.method === 'DELETE' && req.url.startsWith('/doc/')) {
+  // Extract employee ID from the URL
+  const id = req.url.split('/')[2];
+
+  // Delete the employee from the database
+  db.query('DELETE FROM EMPLOYEE WHERE EID = ?', [id], (error, results) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Internal Server Error' }));
+      } else {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ message: 'Employee deleted successfully' }));
+      }
+  });
+} else if (req.url === '/doc') {
+  // Fetch employee details from MySQL database
+  db.query('SELECT EID, FName, LName, Contact, Gender, PhoneNum FROM DOCTOR', (error, results, fields) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Internal Server Error' }));
+          return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(results));
+  });
+}
+/*Add Doctors */
+else if (req.method === 'POST' && req.url=== '/addDoc') {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const data = JSON.parse(body);
+
+    const {eid, fname, lname, email, gender, number, role } = data;
+    // Insert data into MySQL table
+    db.query('INSERT INTO DOCTOR (EID, FName, LName, Contact, Gender, PhoneNum, Department) VALUES (?, ?, ?, ?, ?, ?, ?)', [eid, fname, lname, email, gender, number, role], (err, result) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Error saving data' }));
+        throw err;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Data saved successfully' }));
+    });
+  });
+}
+/*View Doctors */
+else if (req.method === 'DELETE' && req.url.startsWith('/Sapp/')) {
+  // Extract employee ID from the URL
+  const id = req.url.split('/')[2];
+
+  // Delete the employee from the database
+  db.query('DELETE FROM EMPLOYEE WHERE EID = ?', [id], (error, results) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Internal Server Error' }));
+      } else {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ message: 'Employee deleted successfully' }));
+      }
+  });
+} else if (req.url === '/Sapp') {
+  // Fetch employee details from MySQL database
+  db.query('SELECT PATIENT_PID, FName, LName, Gender, Date, Time FROM APPOINTMENT', (error, results, fields) => {
+      if (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Internal Server Error' }));
+          return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(results));
+  });
+}
 else {
   res.writeHead(404, { 'Content-Type': 'text/plain' });
   res.end('Not Found');
 }
 });
+/*Show Appointments in appointment tab*/
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
